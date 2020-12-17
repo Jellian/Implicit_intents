2e22bb2502e4d97509c312def54c14b4d10a04fd
Jellian <haanniiaarodriguez@gmail.com>
2020-12-16 22:56:08 -0600

    se añadió método para enviar mensaje por distintas plataformas

--- app/src/main/java/com/fiuady/implicit_intents/MainActivity.kt
+++ app/src/main/java/com/fiuady/implicit_intents/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.fiuady.implicit_intents
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharetext: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharetext= findViewById(R.id.text_share);
+
+    }
+
+    fun shareText(view: View) {
+
+        val txt:String =sharetext.text.toString()
+        val mimeType = "text/plain"
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with: ")
+                .setText(txt)
+                .startChooser();
     }
 }
